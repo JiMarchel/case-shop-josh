b8cedb76a5f90c926e2fc6e527218496a0736e3d
JiMarchel <jmarchel100@gmail.com>
2024-05-20 15:39:33 +0700

    design page

---
+++ app/(validator)/options-validator.ts
@@ -0,0 +1,43 @@
+// bg-zinc-950 border-zinc-950
+// bg-blue-950 border-blue-950
+// bg-rose-950 border-rose-950
+
+export const COLORS = [
+  { label: "Black", value: "black", tw: "bg-zinc-950" },
+  {
+    label: "Blue",
+    value: "blue",
+    tw: "bg-blue-950",
+  },
+  { label: "Rose", value: "rose", tw: "bg-rose-950" },
+] as const;
+
+export const MODELS = {
+  name: "models",
+  options: [
+    {
+      label: "iPhone X",
+      value: "iphonex",
+    },
+    {
+      label: "iPhone 11",
+      value: "iphone11",
+    },
+    {
+      label: "iPhone 12",
+      value: "iphone12",
+    },
+    {
+      label: "iPhone 13",
+      value: "iphone13",
+    },
+    {
+      label: "iPhone 14",
+      value: "iphone14",
+    },
+    {
+      label: "iPhone 15",
+      value: "iphone15",
+    },
+  ],
+} as const;
